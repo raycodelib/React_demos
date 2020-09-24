8edef8d192d5f593fd60be7376e31ba66db5ab2f
lei xie <xielei8912@outlook.com>
2020-09-24 22:24:21 +1000

    first commit

---
+++ hello-react/src/components/app/app.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import CommentAdd from '../comment-add/comment-add';
+import CommentList from '../comment-list/comment-list';
+
+export default class App extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         comments: [
+    //             { username: "Jack", content: "React is awesome!!" },
+    //             { username: "Mike", content: "I like React" },
+    //             { username: "Tom", content: "React is difficult" },
+    //         ],
+    //     };
+    // }
+    state = {
+        comments: [
+            { username: 'Jack', content: 'React is awesome!!' },
+            { username: 'Mike', content: 'I like React' },
+            { username: 'Tom', content: 'React is difficult' },
+        ],
+    };
+
+    addComment = (comment) => {
+        const { comments } = this.state;
+        comments.unshift(comment);
+        this.setState({ comments });
+    };
+
+    deleteComment = (index) => {
+        const { comments } = this.state;
+        comments.splice(index, 1);
+        this.setState({ comments });
+    };
+
+    render() {
+        const { comments } = this.state;
+
+        return (
+            <div>
+                <header className='site-header jumbotron'>
+                    <div className='container'>
+                        <div className='row'>
+                            <div className='col-xs-12'>
+                                <h1>Do you like React?</h1>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+                <div className='container'>
+                    <CommentAdd addComment={this.addComment} />
+                    <CommentList
+                        comments={comments}
+                        deleteComment={this.deleteComment}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
---
+++ hello-react/src/components/comment-list/comment-list.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import CommentItem from '../comment-item/comment-item';
+import './comment-list.css';
+
+export default class CommentList extends Component {
+    static propTypes = {
+        comments: PropTypes.array.isRequired,
+        deleteComment: PropTypes.func.isRequired,
+    };
+
+    render() {
+        const { comments, deleteComment } = this.props;
+        const display = comments.length === 0 ? 'block' : 'none';
+        return (
+            <div className='col-md-8'>
+                <h3 className='reply'>Comments:</h3>
+                <h2 style={{ display }}>
+                    No comment, add your comment via left window
+                </h2>
+                <ul className='list-group'>
+                    {comments.map((comment, index) => (
+                        <CommentItem
+                            comment={comment}
+                            key={index}
+                            deleteComment={deleteComment}
+                            index={index}
+                        />
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+}
+
+// CommentList.propType = {
+//     comments: PropTypes.array.isRequired,
+// };
